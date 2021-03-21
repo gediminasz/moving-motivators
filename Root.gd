fca267fe4cea149ff45999169e66cb40b7f9e918
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2021-03-21 15:08:08 +0200

    Add unique colors to cards

--- Root.gd
+++ Root.gd
@@ -14,49 +14,62 @@ func _ready():
     var cards = [
         {
             "title": "Curiosity",
-            "description": "I have plenty of things to investigate and to think about"
+            "description": "I have plenty of things to investigate and to think about",
+            "color": "FFA500",
         },
         {
             "title": "Acceptance",
-            "description": "The people around me approve of what I do and who I am"
+            "description": "The people around me approve of what I do and who I am",
+            "color": "FFD700",
         },
         {
             "title": "Power",
-            "description": "There’s enough room for me to influence what happens around me"
+            "description": "There’s enough room for me to influence what happens around me",
+            "color": "DAA520",
         },
         {
             "title": "Relatedness",
-            "description": "I have good social contacts with the people in my work"
+            "description": "I have good social contacts with the people in my work",
+            "color": "008000",
         },
         {
             "title": "Goal",
-            "description": "My purpose in life is reflected in the work that I do"
+            "description": "My purpose in life is reflected in the work that I do",
+            "color": "000080",
         },
         {
             "title": "Honor",
-            "description": "I feel proud that my personal values are reflected in how I work"
+            "description": "I feel proud that my personal values are reflected in how I work",
+            "color": "00BFFF",
         },
         {
             "title": "Mastery",
-            "description": "My work challenges my competence but it is still within my abilities"
+            "description": "My work challenges my competence but it is still within my abilities",
+            "color": "008B8B",
         },
         {
             "title": "Freedom",
-            "description": "I am independent of others with my work and my responsibilities"
+            "description": "I am independent of others with my work and my responsibilities",
+            "color": "DC143C",
         },
         {
             "title": "Order",
-            "description": "There are enough rules and policies for a stable environment"
+            "description": "There are enough rules and policies for a stable environment",
+            "color": "FF69B4",
         },
         {
             "title": "Status",
-            "description": "My position is good, and recognized by the people who work with me"
+            "description": "My position is good, and recognized by the people who work with me",
+            "color": "800080",
         },
     ]
 
     for properties in cards:
         var card = Card.instance()
         card.get_node("Title").text = properties["title"]
+        var stylebox = StyleBoxFlat.new()
+        stylebox.bg_color = Color(properties["color"])
+        card.get_node("Title").add_stylebox_override("normal", stylebox)
         card.get_node("Description").text = properties["description"]
         card.position = $Center.position + Vector2(rng.randi_range(-480, 480), rng.randi_range(-300, 300))
         card.add_to_group("cards")
